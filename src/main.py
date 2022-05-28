19c21c1dc5471bf10589b6bf3aabe461a6ce245e
Tatsumi <tatsumi719@gmail.com>
2022-05-29 00:03:35 +0900

    ワイ「ち、チノちゃん！　おちんちん激しくしないで！」　チノ「うるさいですね」ｼｺｼｺｼｺワイ「あ、あぁ～ッ！」　ドピュドピュドピューッ！ チノ「はい、今日の搾精は終わり。お疲れさまでした」 ワイ「うぅあ、ありがとうございました」 数週間前、念願のラビットハウスに就職したのだが、『女性ばかりの街で若い男を野放しにすると皆レイプされるのでは』 という懸念の声があり、結果、チノちゃんが定期的にワイのオチンチンから精子をシコシコしてくれるようになった。しかしチノちゃんはなんだか ワイのことがキライみたいで、いつもいつも不愛想にオチンチンシコシコして、ちんちんイタイイタイなのだった。ワイ「トホホチノちゃん可愛いのにオチンチンシコシコはイタイイタイなんだからあーあ、どうにかしてチノちゃんの精液ピュッピュをやさしくて気持ちいものにしてもらえないかな～、ん？」 深夜なのにチノちゃんの部屋から明かりが漏れている。チノ「よいしょよいしょ」 ワイ（ち、チノちゃんが、自分の部屋でおちんぽディルドを相手にオチンポシコシコの練習をしている！？） チノ「ふぅこんなものですかね。もっと気持ち良くなってもらえるように頑張らないと」 ワイ「チノちゃーん！」　バターンッ！ チノ「ひゃあッ！？」 ワイ「チ、チノちゃーん！　ごめんよーッ！　チノちゃんは毎日ワイのためにオチンチンしこしこの練習してたのにワイはそんなことも知らずにッ！ ハフッ！ハフッ！　チノちゃんの湯上りふとももいい匂い！」 チノ「ど、ドサクサにまぎれて匂いを嗅がないでください！」 ワイ「ご、ごめんねチノちゃん！」 チノ「べ、別に、オチンチンしこしこ練習するくらい普通です。それが私のお仕事なんですから。それに、私は下手で、あんまり気持ちよくなってもらえないから」 ワイ「そ、そんなことないよ！　チノちゃんのその気持ちだけでワイは十分オチンチン気持ちいいんだよ！　あっ、そ、そうだ！　チノちゃんおてて出して！ チノ「こ、こうですか？」 ワイ「そう！　それじゃあ今からオナニーするからね！　チノちゃんのやわらかおててにドッピュするからね！　ちゃんと受け止めてね！」 チノ「えっ、えっ？」 ワイ「ウオーッ！　チノ！　ぷにぷにおててに出すぞ！」ドピュドピュドピューッ！ チノ「ひゃあッ！」ビシャーッワイ「くっ、ふぅ！　す、すっごい濃いのが出たぁーッ！」 チノ「ほんとうですで、でもなんで？」 ワイ「それはねチノちゃんの気持ちが、ワイに伝わったからだよ！　チノちゃんの他人を思いやる優しさがね！」 チノ「私のやさしさ」 ワイ「そう！　だから、テクニックなんて、二の次なんだよ！　オチンチンしこしこは、上手い人にやってもらうより、好きな人にやってもらうのが一番気持ちいいんだよ！」 チノ「す、好きってはわわあ、あのもうちょっとだけ、練習に付き合ってもらってもいいですか？」 ワイ「もちろん！」 その後、ワイは一晩中チノちゃんのおててに射精を続けて次の日の朝は起き上がれないほど疲弊していた。でもまぁ、その日以来、おちんちん搾りをするときチノちゃんが耳元で「好きです」とつぶやいてくれるようになったので結果オーライ！　終わり

--- src/main.py
+++ src/main.py
@@ -20,12 +20,13 @@ for i in range(10):
     number_of_documents = create_n_gram_list.count_documents(input_file)
     n_gram_list.append(create_n_gram_list.create_n_gram_list(input_file, n_gram_number, representation_type, frequency_threshold, number_of_documents))
     
+    print(str(i + 1) + "番目のテキストファイル")
     Zresult = Zgen.search_Z(n_gram_list[i])
     print(Zresult)
     education_result = voca.voca(n_gram_list[i])
     print(education_result)
 # create dictionary of word forms and the number of documents they occur in#
 #語形とそれが出現する文書数の辞書を作成する#
-print(n_gram_list[3])   # これはn4の辞書を表示している 
+#print(n_gram_list[3])   # これはn4の辞書を表示している 
 
 
