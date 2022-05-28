0e45352bbe7204a18154a423e7e9dd69c0855a43
Keisuke Kano <86813026+s1280106kshow@users.noreply.github.com>
2022-05-28 16:29:53 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-data = []
 
 for i in range(1,11):
-    with open('../Data/n{i:>3d}.txt', mode='r') as f:     # データの読み込み 1から10
-        data[i] = f.read()
+    with open(f'../Data/n{i}.txt', mode='r') as f:     # データの読み込み 1から10
+        data = f.read()
