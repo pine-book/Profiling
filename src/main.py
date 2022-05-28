db0a66a9bea815319cf697708a69fad91c1e6467
Keisuke Kano <86813026+s1280106kshow@users.noreply.github.com>
2022-05-28 16:31:41 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 
-for i in range(1,11):
-    with open(f'../Data/n{i}.txt', mode='r') as f:     # データの読み込み 1から10
+for i in range(1,11):   # ループ開始　１から１０
+    with open(f'../Data/n{i}.txt', mode='r') as f:     # データの読み込み
         data = f.read()
