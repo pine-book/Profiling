d49da5a205281978ea6ff1fa755d61458268fcd8
Keisuke Kano <86813026+s1280106kshow@users.noreply.github.com>
2022-05-29 00:17:32 +0900

    commitするときのコメントで遊ばないように。邪魔なので。

--- src/main.py
+++ src/main.py
@@ -13,20 +13,20 @@ frequency_threshold = 0  #謎？おそらく０～１の間で１にちかづく
 input_files = []    #サンプルファイル(n1 ~ n10)のディレクトリのリスト
 n_gram_list = []    #辞書のリスト(n1~ n10まである)
 
-for i in range(1,11):
-    input_files.append(os.path.join('.\\Data\\',"n" + str(i) + '.txt'))
+for i in range(10):
+    input_files.append(os.path.join('.\\Data\\',"n" + str(i+1) + '.txt'))
     input_file = []
-    input_file.append(input_files[i])
+    input_file.append(input_files[i+1])
     number_of_documents = create_n_gram_list.count_documents(input_file)
     n_gram_list.append(create_n_gram_list.create_n_gram_list(input_file, n_gram_number, representation_type, frequency_threshold, number_of_documents))
     
-    print(str(i) + "番目のテキストファイル")
+    print(str(i+1) + "番目のテキストファイル")
     Zresult = Zgen.search_Z(n_gram_list[i])
     print(Zresult)
     education_result = voca.voca(n_gram_list[i])
     print(education_result)
 # create dictionary of word forms and the number of documents they occur in#
 #語形とそれが出現する文書数の辞書を作成する#
-#print(n_gram_list[3])   # これはn4の辞書を表示している 
+#print(n_gram_list[4])   # これはn4の辞書を表示している 
 
 
