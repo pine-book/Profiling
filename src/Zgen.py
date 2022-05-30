f47ece33fa8954123da2bddb85d57d74f8b916dd
Tatsumi <tatsumi719@gmail.com>
2022-05-30 16:33:38 +0900

    os.path.join を本来意図した動作になるように修正

--- src/Zgen.py
+++ src/Zgen.py
@@ -1,9 +1,8 @@
-from asyncio.windows_events import NULL
-from gettext import NullTranslations
 import os
 from re import A
 def search_Z(n_gram_list):
-    with open(os.path.join('.\\Data\\Zgen.txt'), 'r', encoding='UTF-8') as f:
+    path = os.path.join('.', 'Data', 'Zgen.txt')
+    with open(path, 'r', encoding='UTF-8') as f:
         Z_word = f.read().split(',')
         string = "Z世代ではありません"
         for zw in Z_word:
