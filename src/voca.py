b7a6c4f6e38bfe2341c5cc7c01bff0012ee948ae
Keisuke Kano <86813026+s1280106kshow@users.noreply.github.com>
2022-05-30 16:20:58 +0900

    Update voca.py

--- src/voca.py
+++ src/voca.py
@@ -2,11 +2,11 @@ from gettext import ngettext
 
 
 def voca(n_gram_list):
-    vocab = len(n_gram_list)
-    word_num = 0
+    type_sum = len(n_gram_list)
+    token_num = 0
     for i in n_gram_list.values():
         word_num += i
     
-    if 0.7 < vocab/word_num:
+    if 0.7 < type_sum/token_num:
         return "天才"
     return "バカ"
