3639945fdd23b3dc2217fb7884ae806b8f6c1b44
Keisuke Kano <86813026+s1280106kshow@users.noreply.github.com>
2022-06-02 14:22:40 +0900

    コメントを追加(english)

--- src/Zgen.py
+++ src/Zgen.py
@@ -5,14 +5,18 @@ z_dict = []
 
 def init():
     # csv_file = open(csv1, "r", encoding="UTF-8", errors="", newline="")
+    # open the csv file.
     with open(CSV_FILE, "r", encoding="UTF-8", errors="", newline="") as csv1:
+        # load csv file into f.
         f = csv.reader(csv1, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
         # header = next(f)
         for date in f:
+            # add data to z_dict.
             z_dict.append([date[0], date[1]])
 
 
 def search_Z(n_gram_list):
+    # determine the author's age based on authr's vocabulary.
     z_rate = 1.0
     for z_word in z_dict:
         if z_word[0] in n_gram_list:
