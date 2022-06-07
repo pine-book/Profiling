3d1ab2378ba831274a80185140d97555a7e8a154
Keisuke Kano <86813026+kshow0719@users.noreply.github.com>
2022-06-07 16:22:12 +0900

    update

--- src/main.py
+++ src/main.py
@@ -11,10 +11,10 @@ n_gram_list = []    # list of dictionary (n1 ~ n10)
 voca.init() # Initialize elderly word list
 
 for i in range(10):
-    input_files.append(os.path.join('.','Data',"n" + str(i+1) + '.txt'))
-    input_file = []
-    input_file.append(input_files[i])
-    number_of_documents = create_n_gram_list.count_documents(input_file)
+    input_files.append(os.path.join('.','Data',"n" + str(i+1) + '.txt')) # read Data file (n1 ~ n10)
+    input_file = [] # initialize input_file
+    input_file.append(input_files[i])   # add data to input_file
+    number_of_documents = create_n_gram_list.count_documents(input_file)    # count number of data file
     n_gram_list.append(create_n_gram_list.create_n_gram_list(input_file, n_gram_number, representation_type, frequency_threshold, number_of_documents))
     
     print("This is letter number " + str(i + 1) + ".")
