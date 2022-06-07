07ea24284afec562f3c55ce3c618e151acd5e7c5
Tatsumi <tatsumi719@gmail.com>
2022-06-07 17:07:21 +0900

    コメント追加

--- src/main.py
+++ src/main.py
@@ -14,9 +14,10 @@ for i in range(10):
     input_files.append(os.path.join('.','Data',"n" + str(i+1) + '.txt')) # read Data file (n1 ~ n10)
     input_file = [] # initialize input_file
     input_file.append(input_files[i])   # add data to input_file
+    #----------------FROM HERE, We use the code taken from this site(https://github.com/jonathandunn/political_classification/blob/master/!OLD/Feature.Extraction.v.0.13.py )
     number_of_documents = create_n_gram_list.count_documents(input_file)    # count number of data file
     n_gram_list.append(create_n_gram_list.create_n_gram_list(input_file, n_gram_number, representation_type, frequency_threshold, number_of_documents))
-    
+    #----------------Here's the quote so far------------------------------------------------------------------------------
     print("This is letter number " + str(i + 1) + ".")
     # This function indicate whether adult or not 
     print(voca.voca(n_gram_list[i])) # This function display whether the author is smart or not.
